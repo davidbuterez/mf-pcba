4c52da0a2c8e1ea6e114869423a20f9348ceeb9e
David Buterez <davidbuterez@Davids-MacBook-Pro.local>
2023-01-18 17:14:23 +0200

    Add helpful comments to pubchem_retrieve.py

--- pubchem_retrieve.py
+++ pubchem_retrieve.py
@@ -16,7 +16,15 @@ from rdkit.Chem.MolStandardize import rdMolStandardize
 
 
 def listkey_request(aid, sids_or_cids):
-    # Note: code doesn't handle errors or bad requests, etc.
+    # Note: the code doesn't handle errors or bad requests, etc.
+    # We use the PubChem REST API to generate a ListKey request.
+    # This is required in order to obtain long lists of data.
+    # Official documentation:
+    # https://pubchem.ncbi.nlm.nih.gov/docs/pug-rest-tutorial#section=Storing-Lists-on-the-Server
+
+    # This function will help us retrieve rows of the requested
+    # data for a specific PubChem AID.
+
     assert sids_or_cids in ['sids', 'cids']
     listkey_template = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/assay/aid/{aid}/{ids}/JSON?list_return=listkey'
     listkey_response = requests.get(listkey_template.format(aid=aid, ids=sids_or_cids)).json()
@@ -28,11 +36,16 @@ def listkey_request(aid, sids_or_cids):
 
 
 def retrieve_csv_from_listkey(template, size, cid_col='PUBCHEM_CID'):
+    # We will use the PubChem REST API to get 'chunks' (rows) of the requested
+    # data as CSV files.
+
     retrieved = []
 
+    # We download a maximum of 5000 rows at once. Larger values can cause errors.
     for i in tqdm(range(0, size + 1, 5000)):
         retrieved.append(requests.get(template.format(iter_start=i)).content)
 
+    # Each chunk is loaded from the string representation into a dataframe.
     processed = []
     for csv in retrieved:
         processed_csv = pd.read_csv(StringIO(str(csv, 'utf-8')))
@@ -40,11 +53,15 @@ def retrieve_csv_from_listkey(template, size, cid_col='PUBCHEM_CID'):
         processed_csv = processed_csv.dropna(subset=[cid_col])
         processed.append(processed_csv)
 
+    # All chunks are joined into a single dataframe.
     df = pd.concat(processed).reset_index(drop=True)
     return df
 
 
 def retrieve_aid_as_csv(aid):
+    # This functions leverages PubChem's REST API to download the full data
+    # table corresponding to an AID.
+
     print('Retrieving AID data from PubChem...')
     listkey, size = listkey_request(aid, 'sids')
     retrieve_template = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/assay/aid/{aid}/CSV?sid=listkey&listkey={listkey_id}&listkey_start={{iter_start}}&listkey_count=5000'
@@ -60,7 +77,11 @@ def retrieve_aid_as_csv(aid):
 
 
 def get_smiles_from_pubchem_by_cid(aid):
-    print('Retrieving CIDs from PubChem...')
+    # The CSV files downloaded from PubChem using retrieve_aid_as_csv() do not
+    # contain SMILES strings. We use different REST calls to get a SMILES
+    # string for each CID.
+
+    print('Retrieving SMILES from PubChem...')
 
     listkey, size = listkey_request(aid, 'cids')
     retrieve_template = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/listkey/{listkey_id}/property/CanonicalSMILES/CSV?&listkey_start={{iter_start}}&listkey_count=5000'
@@ -71,12 +92,21 @@ def get_smiles_from_pubchem_by_cid(aid):
 
 
 def aggregate_sd(df, list_of_replicate_column_names):
+    # This function aggregates replicate measurements from the primary screen
+    # (given by the different column names, if more than one). The functions
+    # also computed a Z-Score based on the activity values (not currently
+    # used in modelling).
+
     df['SD'] = df[list_of_replicate_column_names].apply(pd.to_numeric, errors='coerce', axis=1).mean(axis=1, skipna=True).astype(float)
     df['SD Z-score'] = stats.zscore(df['SD'].values, nan_policy='omit')
     return df
 
 
 def aggregate_dr(df, list_of_replicate_column_names, transform_dr='no'):
+    # Aggregate multiple measurements (columns) for confirmatory data, if they
+    # exist. The function also takes care of converting a generic 'XC50' score
+    # into the corresponding pXC50.
+
     assert transform_dr in ['no', 'pXC50', 'minus']
 
     processed_dr = df[list_of_replicate_column_names].apply(pd.to_numeric, errors='coerce', axis=1).mean(axis=1, skipna=True)
@@ -97,11 +127,15 @@ def aggregate_dr(df, list_of_replicate_column_names, transform_dr='no'):
 
 
 def reduce_df(df, column_names):
+    # Reformat the dataframe to keep only the CID and the new columns.
+
     col_elems = dict(zip(column_names, ['mean' for _ in range(len(column_names))]))
     return df.groupby('CID').agg({'CID': 'first', **col_elems, 'Activity': 'first'}).reset_index(drop=True)
 
 
 def add_mols_from_smiles(df):
+    # This function generates an RDKit mol object for each molecule.
+
     print('Reading SMILES into RDKit...')
     df['mol'] = [Chem.MolFromSmiles(s) for s in tqdm(df['CanonicalSMILES'])]
     df['rdkit-smiles'] = [Chem.MolToSmiles(m) for m in tqdm(df['mol'])]
@@ -110,7 +144,11 @@ def add_mols_from_smiles(df):
 
 
 def remove_if_different_largest_fragment(df):
+    # Removes from the dataframe molecules that contain a combination of
+    # compounds.
+
     print('Largest fragment filtering...')
+
     largest_Fragment = rdMolStandardize.LargestFragmentChooser()
     df['l-fragsmiles'] = [Chem.MolToSmiles(largest_Fragment.choose(m)) for m in tqdm(df['mol'])]
     df = df.loc[df['rdkit-smiles'] == df['l-fragsmiles']]
@@ -119,10 +157,15 @@ def remove_if_different_largest_fragment(df):
 
 
 def remove_stereoisomers(df, sort_col='SD'):
+    # Removes the stereochemical information from all molecules in a dataframe
+    # and keeps only the unique resulting SMILES.
+
     print('Removing stereoisomers...')
+
     def smiles_no_stereo(mol):
         rdmolops.RemoveStereochemistry(mol)
         return Chem.MolToSmiles(mol)
+    
     df['sf-smiles'] = df['mol'].apply(func=smiles_no_stereo)
     df = df.sort_values(sort_col, ascending=False).drop_duplicates('sf-smiles', keep='first')
 
@@ -130,7 +173,11 @@ def remove_stereoisomers(df, sort_col='SD'):
 
 
 def neutralise_df(df):
+    # Retain only dataframe compounds that can be recorded with a formal
+    # charge of 0.
+
     print('Neutralising molecules...')
+
     def neutralise_smiles(mol):
         try:
             pattern = Chem.MolFromSmarts("[+1!h0!$([*]~[-1,-2,-3,-4]),-1!$([*]~[+1,+2,+3,+4])]")
@@ -147,6 +194,7 @@ def neutralise_df(df):
             return Chem.MolToSmiles(mol)
         except Chem.rdchem.AtomValenceException:
             return None
+        
     df['neut-smiles'] = df['mol'].apply(func=neutralise_smiles)
     df = df[df['neut-smiles'].notna()]
     df['neut-mol'] = [Chem.MolFromSmiles(s) for s in tqdm(df['neut-smiles'])]
@@ -157,14 +205,22 @@ def neutralise_df(df):
 
 
 def filter(aid, list_of_sd_cols, list_of_dr_cols, transform_dr, aid_dr=None):
+    # Main processing function, taking care of downloading and filtering AID
+    # data. Keeps and returns the associated metadata.
+
     metadata = {'AID': aid, 'SD columns': list_of_sd_cols, 'DR columns': list_of_dr_cols, 'Separate DR': 'Yes' if aid_dr else 'No'}
 
+    # Retrieve the AID data table.
     main_df = retrieve_aid_as_csv(aid)
     metadata['Unfiltered SD size'] = main_df.shape[0]
+
+    # If the confirmatory (DR) data is in a separate AID, retrieve it.
     if aid_dr:
         dr_df = retrieve_aid_as_csv(aid_dr)
         metadata['Unfiltered DR size'] = dr_df.shape[0]
 
+    # Clean the resulting dataframe(s) and keep only the CID and activities.
+    # Again, if there is a different AID for DR, this is handled separately.
     if aid_dr:
         main_df = reduce_df(aggregate_sd(main_df, list_of_sd_cols), column_names=['SD', 'SD Z-score'])
         dr_df, dr_names = aggregate_dr(dr_df, list_of_dr_cols, transform_dr=transform_dr)
@@ -175,6 +231,7 @@ def filter(aid, list_of_sd_cols, list_of_dr_cols, transform_dr, aid_dr=None):
         main_df = reduce_df(main_df, column_names=['SD', 'SD Z-score', *dr_names])
         metadata['Unfiltered DR size'] = main_df[main_df['DR'].notna()].shape[0]
 
+    # Add SMILES to the dataframe(s), based on the CIDs.
     main_smiles_df = get_smiles_from_pubchem_by_cid(aid)
     main_df = main_df.merge(right=main_smiles_df, on='CID')
 
@@ -182,6 +239,7 @@ def filter(aid, list_of_sd_cols, list_of_dr_cols, transform_dr, aid_dr=None):
         dr_smiles_df = get_smiles_from_pubchem_by_cid(aid_dr)
         dr_df = dr_df.merge(right=dr_smiles_df, on='CID')
 
+    # Add RDKit mol objects based on the SMILES.
     main_df = add_mols_from_smiles(main_df)
     metadata['After RDKit SD size'] = main_df.shape[0]
     if aid_dr:
@@ -190,6 +248,7 @@ def filter(aid, list_of_sd_cols, list_of_dr_cols, transform_dr, aid_dr=None):
     else:
         metadata['After RDKit DR size'] = main_df[main_df['DR'].notna()].shape[0]
 
+    # Remove CIDs that denoted a combination of compounds.
     main_df = remove_if_different_largest_fragment(main_df)
     metadata['After largest fragment SD size'] = main_df.shape[0]
     if aid_dr:
@@ -198,6 +257,7 @@ def filter(aid, list_of_sd_cols, list_of_dr_cols, transform_dr, aid_dr=None):
     else:
         metadata['After largest fragment DR size'] = main_df[main_df['DR'].notna()].shape[0]
 
+    # Remove stereochemistry
     main_df = remove_stereoisomers(main_df, 'SD')
     metadata['After stereoisomers SD size'] = main_df.shape[0]
     if aid_dr:
@@ -206,6 +266,7 @@ def filter(aid, list_of_sd_cols, list_of_dr_cols, transform_dr, aid_dr=None):
     else:
         metadata['After stereoisomers DR size'] = main_df[main_df['DR'].notna()].shape[0]
 
+    # Keep only compounds that can be recorded with a formal charge of 0.
     main_df = neutralise_df(main_df)
     metadata['After neutralisation SD size'] = main_df.shape[0]
     if aid_dr:
@@ -220,7 +281,7 @@ def filter(aid, list_of_sd_cols, list_of_dr_cols, transform_dr, aid_dr=None):
     if aid_dr:
         assert len(dr_df['CID'].values) == len(set(dr_df['CID'].values.tolist()))
 
-
+    # Make sure to only keep columns of interest.
     if aid_dr:
         main_df = main_df[['CID', 'SD', 'SD Z-score', 'Activity', 'neut-smiles']]
         dr_df = dr_df[['CID', *dr_names, 'Activity', 'neut-smiles']]
@@ -234,6 +295,7 @@ def filter(aid, list_of_sd_cols, list_of_dr_cols, transform_dr, aid_dr=None):
         merged_df = merged_df.rename(columns={'Activity_y': 'Activity', 'neut-smiles_y': 'neut-smiles'})
 
 
+    # Separate compounds with a DR value but without an SD value.
     dr_not_in_sd_df = None
     if not aid_dr:
         main_dr_not_sd = main_df[(main_df['SD'].isna()) & (main_df['DR'].notna())]
@@ -251,6 +313,7 @@ def filter(aid, list_of_sd_cols, list_of_dr_cols, transform_dr, aid_dr=None):
             dr_not_in_sd_df = dr_df[dr_df['CID'].isin(dr_cids_not_in_sd)]
 
 
+    # Update metadata with additional statistics.
     metadata['Filtered SD size'] = main_df.shape[0]
     pd.set_option('use_inf_as_na', True)
     if aid_dr:
@@ -263,11 +326,8 @@ def filter(aid, list_of_sd_cols, list_of_dr_cols, transform_dr, aid_dr=None):
         metadata['Filtered DR size'] = main_df_dr.shape[0]
         r, pvalue = stats.pearsonr(main_df_dr[main_df_dr['DR'].notna()]['SD'].values, main_df_dr[main_df_dr['DR'].notna()]['DR'].values)
 
-    r, pvalue = 0, 0
-
     metadata['Pearson r'] = float(r)
     metadata['Pearson correlation p-value'] = float(pvalue)
-
 
     return main_df, merged_df, dr_not_in_sd_df, metadata
 
@@ -287,20 +347,25 @@ def main():
     args = parser.parse_args()
     argsdict = vars(args)
 
+    # Get SD and DR dataframes (if separate), a dataframe of DR compounds with
+    # no SD values (if it exists), and the computed metadata.
     main_df, dr_df, dr_not_in_sd_df, metadata = filter(argsdict['AID'], list_of_sd_cols=argsdict['list_of_sd_cols'], list_of_dr_cols=argsdict['list_of_dr_cols'], transform_dr=argsdict['transform_dr'], aid_dr=argsdict['AID_DR'])
 
+    # Generate a save directory at the indicated path.
     if argsdict['AID_DR']:
         save_dir = '{save_dir}/AID{id2}-{id}/'.format(save_dir=argsdict['save_dir'], id=argsdict['AID'], id2=argsdict['AID_DR'])
     else:
         save_dir = '{save_dir}/AID{id}/'.format(save_dir=argsdict['save_dir'], id=argsdict['AID'])
     Path(save_dir).mkdir(exist_ok=True)
 
+    # Save dataframes to disk.
     main_df.reset_index(drop=True).to_csv(f'{save_dir}/SD.csv', index=False)
     if argsdict['AID_DR']:
         dr_df.reset_index(drop=True).to_csv(f'{save_dir}/DR.csv', index=False)
     if dr_not_in_sd_df is not None and not dr_not_in_sd_df.empty:
         dr_not_in_sd_df.reset_index(drop=True).to_csv(f'{save_dir}/DR_not_in_SD.csv', index=False)
 
+    # Save metadata to disk.
     with open(f'{save_dir}/metadata.json', 'w') as file_out:
         json.dump(metadata, file_out)
 
